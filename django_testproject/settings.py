20480f65d245ab25881aef609435413fc3e0e57e
yamierite <darkote666@gmail.com>
2020-09-01 00:08:23 +0300

    wip deployment on heroku

--- django_testproject/settings.py
+++ django_testproject/settings.py
@@ -20,13 +20,15 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
-SECRET_KEY ='o%vk2te2m(opdvfm4)pz6ejjr6gpt%gb01u4b^(6u#2=)v*)2+'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'djangoblogtesting.herokuapp.com'
+    ]
 
 
 # Application definition
